469d36b1fad41268c0e1deb56bafb6718370b30a
Mohammad Sakhidel <mohammad.sakhidel@gmail.com>
2017-12-17 19:12:23 +0330

    kiosk app structure created.

---
+++ SamPresentationLayer/SamKiosk/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SamUxLib.Code.Utils;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WPFTabTip;
+
+namespace SamKiosk
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                TabTipAutomation.BindTo<TextBox>();
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.Handle(ex);
+            }
+
+            base.OnStartup(e);
+        }
+    }
+}
--- SamPresentationLayer/SamUxLib/Resources/Values/Enums.Designer.cs
+++ SamPresentationLayer/SamUxLib/Resources/Values/Enums.Designer.cs
@@ -19,7 +19,7 @@ namespace SamUxLib.Resources.Values {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Enums {
@@ -241,7 +241,7 @@ namespace SamUxLib.Resources.Values {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to مجلس یادبود.
+        ///   Looks up a localized string similar to شام غریبان.
         /// </summary>
         public static string ObitType_yadbood {
             get {
