b16659171436f6326fadfa33e629a12525b6984b
Mohammad Sakhidel <mohammad.sakhidel@gmail.com>
2018-02-27 14:18:57 +0330

    consolations report added to desktop application.

---
+++ SamLibrary/SamReportLib/Models/ConsolationRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SamReportLib.Models
+{
+    public class ConsolationRecord
+    {
+        public int ID { get; set; }
+        public string MosqueName { get; set; }
+        public string ObitTitle { get; set; }
+        public string CustomerCellPhone { get; set; }
+        public int Amount { get; set; }
+        public string TemplateTitle { get; set; }
+        public string Content { get; set; }
+        public string Status { get; set; }
+        public string TrackingNumber { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
--- SamLogicLayer/SamAPI/Controllers/ReportsController.cs
+++ SamLogicLayer/SamAPI/Controllers/ReportsController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using SamAPI.Code.Utils;
 
 namespace SamAPI.Controllers
 {
@@ -29,27 +30,73 @@ namespace SamAPI.Controllers
         [HttpPost]
         public IHttpActionResult MosqueTurnoverRecords(MosquesTurnoverQuery query)
         {
-
-            #region retrieve data:
-            var records = _mosqueRepo.GetMosquesTurnover(query.BeginDate, query.EndDate, query.ProvinceID, query.CityID, query.MosqueID);
-            #endregion
-
-            #region prepare report model:
-            var dataItems = new List<MosqueTurnoverRecord>();
-            foreach (var r in records)
+            try
             {
-                var item = new MosqueTurnoverRecord();
-                item.MosqueName = r.Item1.Name;
-                item.ObitsCount = r.Item2.Count();
-                item.ConsolationCount = r.Item3.Count();
-                item.ConsolationAverage = item.ObitsCount > 0 ? (double)item.ConsolationCount / item.ObitsCount : 0;
-                item.TotalIncome = (int)r.Item3.Sum(c => c.AmountToPay);
+                #region retrieve data:
+                var records = _mosqueRepo.GetMosquesTurnover(query.BeginDate, query.EndDate, query.ProvinceID, query.CityID, query.MosqueID);
+                #endregion
 
-                dataItems.Add(item);
+                #region prepare report model:
+                var dataItems = new List<MosqueTurnoverRecord>();
+                foreach (var r in records)
+                {
+                    var item = new MosqueTurnoverRecord
+                    {
+                        MosqueName = r.Item1.Name,
+                        ObitsCount = r.Item2.Count(),
+                        ConsolationCount = r.Item3.Count()
+                    };
+                    item.ConsolationAverage = item.ObitsCount > 0 ? (double)item.ConsolationCount / item.ObitsCount : 0;
+                    item.TotalIncome = (int)r.Item3.Sum(c => c.AmountToPay);
+
+                    dataItems.Add(item);
+                }
+                #endregion
+
+                return Ok(dataItems.OrderBy(d => d.MosqueName));
             }
-            #endregion
+            catch (Exception ex)
+            {
+                return ResponseMessage(ExceptionManager.GetExceptionResponse(this, ex));
+            }
+        }
 
-            return Ok(dataItems.OrderBy(d => d.MosqueName));
+        [HttpPost]
+        public IHttpActionResult ConsolationsReport(ConsolationsQuery query)
+        {
+            try
+            {
+                #region retrieve data:
+                var records = _consolationRepo.Query(query.BeginDate, query.EndDate, query.ProvinceID, 
+                    query.CityID, query.MosqueID, query.Status, query.CustomerCellphone, query.TrackingNumber);
+                #endregion
+
+                #region prepare report model:
+                var dataItems = new List<ConsolationRecord>();
+                foreach (var c in records)
+                {
+                    dataItems.Add(new ConsolationRecord()
+                    {
+                        ID = c.ID,
+                        Status = c.Status,
+                        TrackingNumber = c.TrackingNumber,
+                        Amount = (int)c.AmountToPay,
+                        Content = c.TemplateInfo,
+                        CreationTime = c.CreationTime,
+                        CustomerCellPhone = c.Customer.CellPhoneNumber,
+                        MosqueName = c.Obit.Mosque.Name,
+                        ObitTitle = c.Obit.Title,
+                        TemplateTitle = c.Template.Name
+                    });
+                }
+                #endregion
+
+                return Ok(dataItems.OrderBy(d => d.CreationTime));
+            }
+            catch (Exception ex)
+            {
+                return ResponseMessage(ExceptionManager.GetExceptionResponse(this, ex));
+            }
         }
         #endregion
 
